9790d7e88c7692846d09e8a95d2f8fcff5e142c5
WMW <mybiggold@naver.com>
2020-06-16 13:32:25 +0900

    클라이언트 템플릿 추가

---
+++ client/src/config.js
@@ -0,0 +1,21 @@
+const colors = {
+  blue: '#1870DC',
+  green: '#58D777',
+  orange: '#f0af03',
+  red: '#F45722',
+  purple: '#474D84',
+  dark: '#040620',
+  teal: '#14d3d3',
+  pink: '#e671b8',
+  gray: '#d6dee5',
+  default: '#595d78',
+  textColor: '#b4b5bf',
+  gridLineColor: '#040620'
+};
+
+export default {
+  colors: {
+    ...colors,
+    white: "#fff"
+  },
+};
