2f4ff91fbd3a265806593b0f5f30fc57a867c9bf
manggong <jihwank1995@gmail.com>
2020-06-22 23:52:48 +0900

    Apply vuetify

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import ParkingLot from "../views/ParkingLot.vue";
+import Contact from "../views/Contact.vue";
 
 Vue.use(VueRouter);
 
@@ -8,24 +10,24 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/parkingLot",
     name: "ParkingLot",
-    component: ParkingLot,
+    component: ParkingLot
   },
   {
     path: "/contact",
     name: "Contact",
-    component: Contact,
-  },
+    component: Contact
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
