9d07b54af7f61c1af5f91427567d3bd84d86ab26
manggong <jihwank1995@gmail.com>
2020-06-22 18:14:32 +0900

    Add Pages (router links)

---
+++ client/src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>컨택트 컴포넌트</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
+
+<style></style>
---
+++ client/src/views/ParkingLot.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>주차장서치(ParkingLot) 컴포넌트</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ParkingLot",
+};
+</script>
+
+<style></style>
