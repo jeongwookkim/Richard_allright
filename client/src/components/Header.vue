fbc46362e08ce6268cad67900fa355c1ba51248d
manggong <jihwank1995@gmail.com>
2020-06-22 20:07:56 +0900

    Modify components, pages

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>헤더 컴포넌트</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
