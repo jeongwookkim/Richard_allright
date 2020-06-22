3e0e7aed3b09348af985adcedbf3feed5e646e7c
manggong <jihwank1995@gmail.com>
2020-06-22 17:28:49 +0900

    Add vue client init

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <router-view />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
-  created() {
-    const currentPath = this.$router.history.current.path;
-
-    if (window.localStorage.getItem('authenticated') === 'false') {
-      this.$router.push('/login');
-    }
-
-    if (currentPath === '/' || currentPath === '/app') {
-      this.$router.push('/app/dashboard');
-    }
-  },
-};
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-<style src="./styles/theme.scss" lang="scss" />
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
