c24d041aa3cdcc0a0b0cb800a6411594d1a37c63
manggong <jihwank1995@gmail.com>
2020-07-01 01:17:11 +0900

    Add kakaoPay API, code refactoring

--- vue-client/src/views/sections/ConfirmPay.vue
+++ vue-client/src/views/sections/ConfirmPay.vue
@@ -14,7 +14,8 @@
   </base-section>
 </template>
 <script>
-// import axios from "axios";
+  // import axios from 'axios'
+  import rp from 'request-promise'
   import { mapGetters } from 'vuex'
   import router from '../../router'
   export default {
@@ -33,7 +34,90 @@
         router.push('/')
       }
     },
-    methods: { payForKakaoPay () {} },
+    methods: {
+      payForKakaoPay () {
+        // const baseUrl = 'https://kapi.kakao.com/v1/payment/ready'
+        // const config = {
+        //   headers: {
+        //     Authorization: `KakaoAK ${process.env.VUE_APP_KAKAO_ADMIN_KEY}`,
+        //     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        //     'Access-Control-Allow-Origin': '*',
+        //   },
+        //   withCredentials: true,
+        //   credentials: 'same-origin',
+        // }
+        // const data = {
+        // cid: 'TC0ONETIME',
+        // partner_order_id: '1111', // 주문번호
+        // partner_user_id: 'partner_user_id', // 유저 아이디
+        // item_name: this.paymentInfo.parkingLotName, // "초코파이", // 메인 주문 상품 외 2건
+        // quantity: 1, // 총 갯수
+        // total_amount: this.paymentInfo.price, // 2200, // 총액
+        // vat_amount: 0,
+        // tax_free_amount: 0,
+        // approval_url: 'https://modoo.herokuapp.com/pay',
+        // fail_url: 'https://www.daum.net',
+        // cancel_url: 'https://www.kakao.com',
+        // }
+        // axios.defaults.withCredentials = true
+        // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+        // axios.post(baseUrl, data, config)
+        //   .then((response) => {
+        //     console.log(response.data)
+        //   })
+        //   .catch((err) => {
+        //     console.log(err)
+        //   })
+
+        const options = {
+          method: 'POST',
+          uri: 'https://kapi.kakao.com/v1/payment/ready',
+          form: {
+            cid: 'TC0ONETIME',
+            partner_order_id: '1111', // 주문번호
+            partner_user_id: 'partner_user_id', // 유저 아이디
+            item_name: this.paymentInfo.parkingLotName, // "초코파이", // 메인 주문 상품 외 2건
+            quantity: 1, // 총 갯수
+            total_amount: this.paymentInfo.price, // 2200, // 총액
+            vat_amount: 0,
+            tax_free_amount: 0,
+            approval_url: 'https://modoo.herokuapp.com/pay',
+            fail_url: 'https://www.daum.net',
+            cancel_url: 'https://www.kakao.com',
+          },
+          headers: {
+            authorization: `KakaoAK ${process.env.VUE_APP_KAKAO_ADMIN_KEY}`,
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        }
+
+        rp(options)
+          .then(async (body) => {
+            console.log(body)
+            // const kakaoResponse = JSON.parse(body)
+            // try {
+            //   const insertOrder = await Order.create({
+            //     quantity,
+            //     order_number,
+            //     customer_id: userId,
+            //     product_id,
+            //   })
+            //   const increaseCount = await Customer.update(
+            //     {
+            //       order_count: userOrderCount,
+            //     },
+            //     { where: { id: userId } },
+            //   )
+            // } catch (err) {
+            //   console.log(err)
+            // }
+            // res.json({ message: kakaoResponse.next_redirect_pc_url })
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      },
+    },
   }
 </script>
 
